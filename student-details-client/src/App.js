4ea805d402735bfed961a28f83b0fdf4d6d0261c
mdYeasin-cmd <yeasin200011@gmail.com>
2023-01-18 07:57:35 +0600

    Tailwind CSS and GitHub configuration added

--- student-details-client/src/App.js
+++ student-details-client/src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <h2>Hello Bangladesh</h2>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 }
